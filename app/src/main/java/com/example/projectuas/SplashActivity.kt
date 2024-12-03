2ed041d00bf0138d200578289dca3b33f2e3a062
Dii ez <127297402+Rekmast@users.noreply.github.com>
2024-12-03 19:52:10 +0700

    splash screen & icon

---
+++ app/src/main/java/com/example/projectuas/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.projectuas
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        val mainView = findViewById<ConstraintLayout>(R.id.main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Menggunakan Handler untuk menunda perpindahan ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 ms = 3 detik
+    }
+}
